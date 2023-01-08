909743871fffb0cecddd57edd4c404f1ef1df041
Polina <75391420+wezbicka@users.noreply.github.com>
2023-01-08 18:15:49 +0300

    Initial commit

---
+++ property/admin.py
@@ -0,0 +1,10 @@
+from django.contrib import admin
+
+from .models import Flat
+
+
+class FlatAdmin(admin.ModelAdmin):
+    search_fields = ('town','address','owner')
+
+
+admin.site.register(Flat, FlatAdmin)
