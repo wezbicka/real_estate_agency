320b4a91f5584d6878d2cceed77253ed9f30a638
Polina <75391420+wezbicka@users.noreply.github.com>
2023-01-31 22:00:14 +0300

    Added Owner model

--- property/admin.py
+++ property/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 
-from .models import Flat, Complaint
+from .models import Flat, Complaint, Owner
 
 
 class FlatAdmin(admin.ModelAdmin):
@@ -23,5 +23,10 @@ class ComplaintAdmin(admin.ModelAdmin):
     raw_id_fields = ('author', 'flat')
 
 
+class OwnerAdmin(admin.ModelAdmin):
+    raw_id_fields = ('flats',)
+
+
 admin.site.register(Flat, FlatAdmin)
 admin.site.register(Complaint, ComplaintAdmin)
+admin.site.register(Owner, OwnerAdmin)
--- property/models.py
+++ property/models.py
@@ -83,3 +83,21 @@ class Complaint(models.Model):
 
     def __str__(self):
         return f'{self.flat} {self.complaint_text}'
+
+
+class Owner(models.Model):
+    name = models.CharField('ФИО владельца', max_length=200)
+    phonenumber = models.CharField('Номер владельца', max_length=20)
+    pure_phone = PhoneNumberField(
+        'Нормализованный номер владельца',
+        blank=True,
+        null=True,
+    )
+    flats = models.ManyToManyField(
+        Flat,
+        related_name='owners',
+        verbose_name="Квартиры в собственности",
+    )
+
+    def __str__(self):
+        return self.owner
