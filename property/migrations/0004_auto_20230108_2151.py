4aaa09e2e720c9bf18b3ca69a5012656483792ed
Polina <75391420+wezbicka@users.noreply.github.com>
2023-02-10 13:07:24 +0300

    Fixed old building

--- property/migrations/0004_auto_20230108_2151.py
+++ property/migrations/0004_auto_20230108_2151.py
@@ -6,6 +6,7 @@ from django.db import migrations
 def set_new_building(apps, schema_editor):
     Flat = apps.get_model('property', 'Flat')
     Flat.objects.filter(construction_year__gte=2015).update(new_building=True)
+    Flat.objects.filter(construction_year__lt=2015).update(new_building=False)
 
 
 class Migration(migrations.Migration):
